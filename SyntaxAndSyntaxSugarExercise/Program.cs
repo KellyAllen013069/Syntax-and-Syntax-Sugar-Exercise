f9e07effa206463ab06da7fd941cbb1e831e134e
KellyAllen013069 <89815777+KellyAllen013069@users.noreply.github.com>
2024-07-10 15:17:56 -0500

    syntax exercise complete

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -2,9 +2,12 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter an integer: ");
+            var answer = int.Parse(Console.ReadLine());
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(response);
         }
     }
 }
